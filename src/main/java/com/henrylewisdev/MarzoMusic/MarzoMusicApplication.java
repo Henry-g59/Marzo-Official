3bb378ec67dec49a025e6ecd1735a9e481851f1b
Henry L <henry.lewisg59@gmail.com>
2022-08-30 00:54:10 -0500

    Init Spring Commit

---
+++ src/main/java/com/henrylewisdev/MarzoMusic/MarzoMusicApplication.java
@@ -0,0 +1,13 @@
+package com.henrylewisdev.MarzoMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarzoMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarzoMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henrylewisdev/MarzoMusic/MarzoMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrylewisdev.MarzoMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarzoMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
